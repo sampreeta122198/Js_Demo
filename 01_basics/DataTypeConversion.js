01df221bf7ce0f9165b2b6a063b5d52fd5a85d5a
sampreeta122198 <144436023+sampreeta122198@users.noreply.github.com>
2024-02-09 13:28:27 +0000

    data type conversion

---
+++ 01_basics/DataTypeConversion.js
@@ -0,0 +1,33 @@
+let score = null
+ let scoreNumber = Number(score)
+ //console.log(scoreNumber);
+//console.log(typeof scoreNumber)
+
+// 33=> 33, number
+// "33abc" => Nan, number
+// "Hitesh" ==> Nan
+//true = 1
+// null = 0
+//undefined = nan
+
+
+let isLogged = "hitesh"
+let booleanIsLogged = Boolean(isLogged)
+//console.log(typeof booleanIsLogged)
+//console.log(booleanIsLogged);
+
+// null => false, boolean
+//1 = true
+// undefined = false
+//"" = false
+//"sam" = true
+
+
+let numberC = null
+
+let numberString = String(numberC)
+
+console.log(typeof numberString);
+console.log(numberString);
+//33 = 33, string
+//null , string
